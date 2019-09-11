ed8232a9a71dc470dd030bdea0d58ba2d4bafb38
Addison Armstrong <addison-armstrong@uiowa.edu>
2019-09-11 00:46:19 -0500

    Finished Program, need comments

--- oral_exam1/S4_ChangeComputation/src/Easy.java
+++ oral_exam1/S4_ChangeComputation/src/Easy.java
@@ -9,169 +9,117 @@ public class Easy {
         this.cents = c;
     }
 
-    public static Easy centsChecker(String check){
-        int i = 0;
-        int j = 0;
+    private static Easy moneyChecker(String check){
         Easy checkTotal = new Easy(0,0);
         Scanner readIn = new Scanner(System.in);
 
         if(check.contains(".")) {
             String[] checkSplit = check.split("\\.");
 
-            checkTotal.dollars = Integer.valueOf(checkSplit[0]);
-            checkTotal.cents = Integer.valueOf(checkSplit[1]);
-            while (checkTotal.cents > 99 || checkTotal.cents < 0 || checkTotal.dollars < 0)  {
+            checkTotal.dollars = Integer.parseInt(checkSplit[0]);
+            checkTotal.cents = Integer.parseInt(checkSplit[1]);
+            while (checkTotal.cents > 99 || checkTotal.cents < 0
+                    || checkTotal.dollars < 0)  {
                 System.out.println("Please re-enter a new total");
                 check = readIn.nextLine();
-
                 if (check.contains(".")) {
                     checkSplit = check.split("\\.");
 
-                    checkTotal.dollars = Integer.valueOf(checkSplit[0]);
-                    checkTotal.cents = Integer.valueOf(checkSplit[1]);
+                    checkTotal.dollars = Integer.parseInt(checkSplit[0]);
+                    checkTotal.cents = Integer.parseInt(checkSplit[1]);
 
-                } else {
-                    checkTotal.dollars = Integer.valueOf(check);
+                } else{
+                    checkTotal.dollars = Integer.parseInt(check);
                     checkTotal.cents = 0;
                 }
             }
-
         } else{
-            checkTotal.dollars = Integer.valueOf(check);
+            checkTotal.dollars = Integer.parseInt(check);
+        }
+        return checkTotal;
+    }
+
+    private static Easy difference(Easy total, Easy given){
+        Scanner readIn = new Scanner(System.in);
+        String cash;
+
+        while(total.dollars > given.dollars ||
+                (total.dollars == given.dollars && total.cents > given.cents)){
+            System.out.println("You did not give enough to pay for the total." +
+                    "\n Please enter in cash given.");
+            cash = readIn.nextLine();
+            given = moneyChecker(cash);
         }
 
-        return checkTotal;
+        if(given.cents < total.cents){
+            given.dollars--;
+            given.cents += 100;
+        }
 
+        given.dollars -= total.dollars;
+        given.cents -= total.cents;
+
+        return given;
+    }
+
+    private static void change(Easy change){
+        int temp = 0;
+        int[] dollarArray = new int[]{20,10,5,1};
+        int[] centArray = new int[]{25,10,5,1};
+
+        for(int i = 0; i < 4 ; i++){
+            temp = change.dollars/dollarArray[i];
+            System.out.println(dollarArray[i] + "'s: " + temp);
+            change.dollars -= temp*dollarArray[i];
+        }
+
+        for(int i = 0; i < 4 ; i++){
+            temp = change.cents/centArray[i];
+            System.out.println("." + centArray[i] + "'s: " + temp);
+            change.cents -= temp*centArray[i];
+        }
     }
     public static void main(String[] args){
-
         Scanner readIn = new Scanner(System.in);
-        int dollars = 0;
-        int cents = 0;
-        int temp = 0;
-        String purchase;
-        String given;
+        String totalString;
+        String givenString;
+        Easy total = new Easy(0,0);
+        Easy given = new Easy(0,0);
 
-
-
-
-        System.out.println("Welcome to S4_ChangeComputation \n" +
+        System.out.println("Welcome to S4_ChangeComputation Made by Addison Armstrong" +
+                " in Intro to Digital Design \n" +
                 "Please enter in the total price of all the items.");
-
-        purchase = readIn.nextLine();
-
-        Easy total = centsChecker(purchase);
-
-
-
-//        if(purchase.contains(".")) {
-//            String[] purchaseSplit = purchase.split("\\.");
-//
-//            dollars = Integer.valueOf(purchaseSplit[0]);
-//            cents = Integer.valueOf(purchaseSplit[1]);
-//            while (cents > 99 || cents < 0 || dollars < 0)  {
-//                System.out.println("Please re-enter a new total");
-//                purchase = readIn.nextLine();
-//
-//                if (purchase.contains(".")) {
-//                    purchaseSplit = purchase.split("\\.");
-//
-//                    dollars = Integer.valueOf(purchaseSplit[0]);
-//                    cents = Integer.valueOf(purchaseSplit[1]);
-//
-//                } else {
-//                    dollars = Integer.valueOf(purchase);
-//                    cents = 0;
-//                }
-//            }
-//
-//        } else{
-//            dollars = Integer.valueOf(purchase);
-//        }
+        totalString = readIn.nextLine();
+        total = moneyChecker(totalString);
 
         System.out.println("Please enter how much you have gave the cashier");
+        givenString = readIn.nextLine();
+        given = moneyChecker(givenString);
 
-        given = readIn.nextLine();
+        if(total.cents < 10){
+            System.out.println("Total: " + total.dollars + ".0" + total.cents);
+        }
+        else{
+            System.out.println("Total: " + total.dollars + "." + total.cents);
+        }
 
-        Easy cash = centsChecker(given);
+        if(given.cents < 10){
+            System.out.println("Cash Given: " + given.dollars + ".0" + given.cents);
+        }
+        else{
+            System.out.println("Cash Given: " + given.dollars + "." + given.cents);
+        }
 
+        given = difference(total, given);
 
+        if(given.cents < 10){
+            System.out.println("Change Given: " + given.dollars + ".0" + given.cents);
+        }
+        else{
+            System.out.println("Change Given: " + given.dollars + "." + given.cents);
+        }
+        change(given);
 
-
-
-        System.out.println("Total: " + total.dollars + "." + total.cents);
-        System.out.println("Cash Given: " + cash.dollars + "." + cash.cents);
-
-//        if(purchase.contains(".")) {
-//            String[] purchaseSplit = purchase.split("\\.");
-//
-//            dollars = Integer.valueOf(purchaseSplit[0]);
-//            cents = Integer.valueOf(purchaseSplit[1]);
-//            while (cents > 99 || cents < 0 || dollars < 0)  {
-//                System.out.println("Please re-enter a new total");
-//                purchase = readIn.nextLine();
-//
-//                if (purchase.contains(".")) {
-//                    purchaseSplit = purchase.split("\\.");
-//
-//                    dollars = Integer.valueOf(purchaseSplit[0]);
-//                    cents = Integer.valueOf(purchaseSplit[1]);
-//
-//                } else {
-//                    dollars = Integer.valueOf(purchase);
-//                    cents = 0;
-//                }
-//            }
-//
-//        } else{
-//            dollars = Integer.valueOf(purchase);
-//        }
-
-
-//        if(dollars/20 > 0){
-//            System.out.println("20's: " + dollars/20);
-//            temp = dollars/20;
-//            dollars = dollars - temp*20;
-//        }
-//        if(dollars/10 > 0){
-//            System.out.println("10's: " + dollars/10);
-//            temp = dollars/10;
-//            dollars = dollars - temp*10;
-//        }
-//        if(dollars/5 > 0){
-//            System.out.println("5's: " + dollars/5);
-//            temp = dollars /5;
-//            dollars = dollars - temp*5;
-//        }
-//
-//        System.out.println("1/s: " + dollars);
-//
-//        if(cents/25 > 0){
-//            System.out.println(".25's: " + cents/25);
-//            temp = cents/25;
-//            cents = cents - temp*25;
-//        }
-//        if (cents/10 > 0){
-//            System.out.println(".10's: " + cents/10);
-//            temp = cents/10;
-//            cents = cents - temp*10;
-//        }
-//        if(cents/5 > 0){
-//            System.out.println(".05's: " + cents/5);
-//            temp = cents/5;
-//            cents = cents - temp*5;
-//        }
-//
-//        System.out.println(".01's: " + cents);
-//
-////        System.out.println("5's: " + dollars/5);
-////        System.out.println("10's: " + dollars/10);
-////        System.out.println("10's: " + dollars/10);
-////        System.out.println("10's: " + dollars/10);
-////        System.out.println("10's: " + dollars/10);
-////        System.out.println("10's: " + dollars/10);
-//
-//        System.out.println(dollars+"\n"+cents);
     }
 
 }
