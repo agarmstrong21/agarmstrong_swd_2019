1247d2848ffd825aca3412294c62d7f675c27a16
Addison Armstrong <addison-armstrong@uiowa.edu>
2019-11-04 16:20:51 -0600

    Added All Javadocs

--- oral_exam2/22-7_RandomCircle_Easy/src/MainMenu.java
+++ oral_exam2/22-7_RandomCircle_Easy/src/MainMenu.java
@@ -2,24 +2,34 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.Random;
 
 public class MainMenu extends JFrame{
     public MainMenu() {
         super("Main Menu");
-        setLayout(new FlowLayout((FlowLayout.CENTER, 5, 5));
+        setLayout(new FlowLayout((FlowLayout.CENTER), 5, 5));
 
         JButton CompPlayerButton = new JButton("Computer Vs. Player");
         JButton PlayerPlayerButton = new JButton("Player Vs. Player");
         add(PlayerPlayerButton);
         add(CompPlayerButton);
 
-        ButtonHandler buttonHandler = new ButtonHandler();
-        PlayerPlayerButton.addActionListener(buttonHandler);
-        CompPlayerButton.addActionListener(buttonHandler);
+        PPBHandler ppbHandler = new PPBHandler();
+        CPBHandler cpbHandler = new CPBHandler();
+        PlayerPlayerButton.addActionListener(ppbHandler);
+        CompPlayerButton.addActionListener(cpbHandler);
     }
 
-    private class ButtonHandler implements ActionListener{
+    private class PPBHandler implements ActionListener{
+        @Override
+        public void actionPerformed(ActionEvent event){
+            RandomCircle textFieldFrame = new RandomCircle();
+            textFieldFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            textFieldFrame.setSize(400,100);
+            textFieldFrame.setVisible(true);
+        }
+    }
+
+    private class CPBHandler implements ActionListener{
         @Override
         public void actionPerformed(ActionEvent event){
 
--- oral_exam2/22-7_RandomCircle_Easy/src/RandomCircle.java
+++ oral_exam2/22-7_RandomCircle_Easy/src/RandomCircle.java
@@ -1,2 +1,15 @@
-public class RandomCircle {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+
+public class RandomCircle extends JFrame {
+
+    private final boolean CompPlayer;
+
+    public RandomCircle(boolean CompPlayer){
+        this.CompPlayer = CompPlayer;
+
+    }
 }
