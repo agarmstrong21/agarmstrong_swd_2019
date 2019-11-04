44199d6cea29b148bce25d532922ca971ed6398f
Addison Armstrong <addison-armstrong@uiowa.edu>
2019-11-04 15:30:20 -0600

    RandomCircle start

---
+++ oral_exam2/22-7_RandomCircle_Easy/src/Driver.java
@@ -0,0 +1,14 @@
+import sun.tools.jar.Main;
+
+import javax.swing.*;
+
+public class Driver extends MainMenu {
+    /* Logic for Random Circle */
+    public static void main(String[] args) {
+        /* Creating the Window of GuessNumber */
+        MainMenu textFieldFrame = new MainMenu();
+        textFieldFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        textFieldFrame.setSize(400,100);
+        textFieldFrame.setVisible(true);
+    }
+}
